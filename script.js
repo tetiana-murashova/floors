79f83c74ffd2bdbb0814c8fb59298dff6cce6877
TetianaMurashova <murashovatania1@gmail.com>
2020-09-29 10:47:23 +1000

    contacts added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector(".scroll-btn").addEventListener("click", () => {
-    document.querySelector("html").style.scrollBehavior = "smooth";
-    setTimeout(() => {
-        document.querySelector("html").style.scrollBehavior = "unset";
-    }, 1000);
-});
+// document.querySelector(".scroll-btn").addEventListener("click", () => {
+//     document.querySelector("html").style.scrollBehavior = "smooth";
+//     setTimeout(() => {
+//         document.querySelector("html").style.scrollBehavior = "unset";
+//     }, 1000);
+// });
 
 
 
