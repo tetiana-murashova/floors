9bebb750ffe29f73aa3e3f3771bac035aec1ec78
TetianaMurashova <murashovatania1@gmail.com>
2020-09-30 15:56:27 +1000

    projects added

--- script.js
+++ script.js
@@ -19,25 +19,59 @@ function mouseOut() {
 }
 
 
-// AOS.init({
-//     // Global settings:
-//     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-//     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-//     initClassName: 'aos-init', // class applied after initialization
-//     animatedClassName: 'aos-animate', // class applied on animation
-//     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-//     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-//     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-//     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+//Gallery
+
+document.getElementById('img1').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/Ultra_Flake_2.jpeg" alt="Ultra Flake" />';
+} )
+document.getElementById('img2').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/Gun_Metal.JPG" alt="Gun Metal" />';
+} )
+document.getElementById('img3').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/Hyper_Flake.jpeg" alt="Hyper Flake" />';
+} )
+document.getElementById('img4').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/Hyper_Flake.4.jpg" alt="Hyper Flake" />';
+} )
+document.getElementById('img5').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/Granite_Small.jpg" alt="Granite Small" />';
+} )
+document.getElementById('img6').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/Ice_White_2.jpg" alt="Ice White" />';
+} )
 //
-//
-//     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-//     offset: 120, // offset (in px) from the original trigger point
-//     delay: 0, // values from 0 to 3000, with step 50ms
-//     duration: 400, // values from 0 to 3000, with step 50ms
-//     easing: 'ease', // default easing for AOS animations
-//     once: false, // whether animation should happen only once - while scrolling down
-//     mirror: false, // whether elements should animate out while scrolling past them
-//     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-//
-// });
+document.getElementById('img7').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0104.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img8').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0102.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img9').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/Suede_Merino_Base.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img10').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0101.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img11').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/Plain_Coat_System.jpg" alt="Plain Coat System" />';
+} )
+document.getElementById('img12').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0108.jpg" alt="Plain Coat System" />';
+} )
+document.getElementById('img13').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0107.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img14').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0106.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img15').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0201.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img16').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0202.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img17').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0203.jpg" alt="Suede Merino Base" />';
+} )
+
