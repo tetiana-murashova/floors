794fff2a095777b9853f53cb3245a0e85bb73d39
TetianaMurashova <murashovatania1@gmail.com>
2020-09-30 19:54:34 +1000

    end of 30/09

--- script.js
+++ script.js
@@ -23,55 +23,74 @@ function mouseOut() {
 //Gallery
 
 document.getElementById('img1').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/Ultra_Flake_2.jpeg" alt="Ultra Flake" />';
+    document.getElementById("big_image").innerHTML = '<img src="images/0101.jpg" alt="Ultra Flake" />';
 } )
 document.getElementById('img2').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/Gun_Metal.JPG" alt="Gun Metal" />';
+    document.getElementById("big_image").innerHTML = '<img src="images/0102.jpg" alt="Gun Metal" />';
 } )
 document.getElementById('img3').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/Hyper_Flake.jpeg" alt="Hyper Flake" />';
+    document.getElementById("big_image").innerHTML = '<img src="images/0103.jpg" alt="Hyper Flake" />';
 } )
 document.getElementById('img4').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/Hyper_Flake.4.jpg" alt="Hyper Flake" />';
+    document.getElementById("big_image").innerHTML = '<img src="images/0104.jpg" alt="Hyper Flake" />';
 } )
 document.getElementById('img5').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/Granite_Small.jpg" alt="Granite Small" />';
+    document.getElementById("big_image").innerHTML = '<img src="images/0105.jpg" alt="Granite Small" />';
 } )
 document.getElementById('img6').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/Ice_White_2.jpg" alt="Ice White" />';
+    document.getElementById("big_image").innerHTML = '<img src="images/0106.jpg" alt="Ice White" />';
 } )
 //
 document.getElementById('img7').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/0104.jpg" alt="Suede Merino Base" />';
-} )
-document.getElementById('img8').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/0102.jpg" alt="Suede Merino Base" />';
-} )
-document.getElementById('img9').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/Suede_Merino_Base.jpg" alt="Suede Merino Base" />';
-} )
-document.getElementById('img10').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/0101.jpg" alt="Suede Merino Base" />';
-} )
-document.getElementById('img11').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/Plain_Coat_System.jpg" alt="Plain Coat System" />';
-} )
-document.getElementById('img12').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/0108.jpg" alt="Plain Coat System" />';
-} )
-document.getElementById('img13').addEventListener('click', ()=> {
     document.getElementById("big_image").innerHTML = '<img src="images/0107.jpg" alt="Suede Merino Base" />';
 } )
-document.getElementById('img14').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/0106.jpg" alt="Suede Merino Base" />';
+document.getElementById('img8').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0108.jpg" alt="Suede Merino Base" />';
 } )
-document.getElementById('img15').addEventListener('click', ()=> {
+document.getElementById('img9').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0109.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img10').addEventListener('click', ()=> {
     document.getElementById("big_image").innerHTML = '<img src="images/0201.jpg" alt="Suede Merino Base" />';
 } )
+document.getElementById('img11').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0202.jpg" alt="Plain Coat System" />';
+} )
+document.getElementById('img12').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0203.jpg" alt="Plain Coat System" />';
+} )
+document.getElementById('img13').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0204.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img14').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0205.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img15').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0206.jpg" alt="Suede Merino Base" />';
+} )
 document.getElementById('img16').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/0202.jpg" alt="Suede Merino Base" />';
+    document.getElementById("big_image").innerHTML = '<img src="images/0207.jpg" alt="Suede Merino Base" />';
 } )
 document.getElementById('img17').addEventListener('click', ()=> {
-    document.getElementById("big_image").innerHTML = '<img src="images/0203.jpg" alt="Suede Merino Base" />';
+    document.getElementById("big_image").innerHTML = '<img src="images/0208.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img18').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0209.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img19').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0301.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img20').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0302.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img21').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0303.jpg" alt="Suede Merino Base" />';
 } )
 
+
+document.getElementById('img22').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0304.jpg" alt="Suede Merino Base" />';
+} )
+document.getElementById('img23').addEventListener('click', ()=> {
+    document.getElementById("big_image").innerHTML = '<img src="images/0305.jpg" alt="Suede Merino Base" />';
+} )
